b5dc184be0bd39f83fc05d4ab0d45d8ba216b3af
Olalekan Isaac Ogunremi <OOgunremi@yahoo.com>
2022-04-27 19:29:22 -0600

    refactor code. Check for completeness

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,12 +1,14 @@
+
 describe("Navigation", () => {
+
   it("should visit root", () => {
     cy.visit("/");
   });
 
   it("should navigate to tuesday",() => {
-  cy.contains("[data-testid=day]", "Tuesday")
-  .click()
-  .should("have.class", "day-list__item--selected");
+
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click().should("have.class", "day-list__item--selected");
 
   });
 });
